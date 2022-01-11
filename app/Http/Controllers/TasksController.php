831fd044b4f6b9a354ad5758aa23680e3e69ce04
tatuyaimada <ti16140907@gmail.com>
2022-01-11 16:55:23 +0000

    CRUD tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    // getでmessages/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return view('tasks.index',[
+            'tasks' =>$tasks,
+            ]);
+    }
+
+    // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+        
+        return view('tasks.create',[
+            'task' =>$task,
+            ]);
+    }
+
+    // postでmessages/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    // getでmessages/（任意のid）にアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        
+        return view('tasks.show',[
+            'task' =>$task,
+            ]);
+    }
+
+    // getでmessages/（任意のid）/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        $task = Task::findOrFail($id);
+        
+        return view('tasks.edit',[
+            'task' =>$task,
+            ]);
+    }
+
+    // putまたはpatchでmessages/（任意のid）にアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    // deleteでmessages/（任意のid）にアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        
+        $task->delete();
+        
+        return redirect('/');
+    }
+}
