fc136866fc4e137021482b9b8f0a849601729ad8
tatuyaimada <ti16140907@gmail.com>
2022-01-31 13:28:08 +0000

    再々提出

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,3 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('tasks', 'TasksController', ['only' => ['index', 'show']]);
-    Route::resource('tasks', 'TasksController', ['only' => ['edit', 'store', 'destroy']]);
-});
